d81f7078993a5c338f5c372666a69f0c852c88a7
Peter Schwier <Peter@Schwier.us>
2025-06-06 16:24:36 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,5 +3,8 @@ describe('chai', function () {
     it('exists', function () {
       chai.assert(chai.assert);
     });
+    it('is a Number (expected to fail)', function () {
+      chai.isNumber(chai.assert);
+    });
   });
 });
